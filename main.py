b378df111324e37657b54d7725ac63d5043381b9
Cherry <138044819+maracujadev@users.noreply.github.com>
2026-07-26 18:32:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands 
 from dotenv import load_dotenv
 import os
+import asyncio
 
 
 load_dotenv() #reading from .env
@@ -9,12 +10,28 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True # for reading messages
+intents.members = True
 
 bot = commands.Bot(command_prefix="#", intents=intents, case_insensitive=True)
+
+######################
+
+### terminal
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
+
+### events
+
+WELCOME_CHANNEL = 1516017173439582229
+
+@bot.event
+async def member_join(member):
+	channel = bot.get_channel(WELCOME_CHANNEL)
+	await channel.send(f"Welcome, {member.mention}!")
+
+### commands
 
 @bot.command()
 async def ping(ctx):
@@ -27,7 +44,7 @@ async def square(ctx, *, number=0):
 
 @bot.command(name="67")
 async def sixtyseven(ctx):
-	await ctx.send("67?")
+	await ctx.send("https://tenor.com/pU4XiE6POTB.gif")
 
 @bot.command()
 async def name(ctx, *, username=None):
@@ -41,5 +58,26 @@ async def name(ctx, *, username=None):
 @bot.command()
 async def poker(ctx):
 	await ctx.send("Poker? I barely know her!")
+
+@bot.command()
+async def remind(ctx, amount: int, unit: str, *, message="something!"):
+	unit = unit.strip().lower()
+
+	if unit.startswith("min"):
+		seconds = amount * 60
+	elif unit.startswith("hour"):
+		seconds = amount * 3600
+	elif unit.startswith("sec"):
+		seconds = amount
+	else:
+		await ctx.send("That didnt work. Try using seconds, minutes or hours.")
+		return
+
+	await ctx.send(f"Will do that! See ya in {amount} {unit}!")
+	await asyncio.sleep(seconds)
+	await ctx.send(f"{ctx.author.mention}, reminder: {message}")
 	
+
+############
+
 bot.run(TOKEN)
