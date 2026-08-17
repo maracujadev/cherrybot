469da145eb64c0b6dc919db4fb7d8ae9f55ce432
maracujadev <metacyclinemachine@gmail.com>
2026-08-17 19:39:55 +0200

    performance improvements

--- main.py
+++ main.py
@@ -1,45 +1,51 @@
-import discord
-from discord.ext import commands 
-from dotenv import load_dotenv
-import os
 import asyncio
 import json
-from datetime import datetime
-import aiohttp
+import os
+from datetime import datetime, timezone
 
-load_dotenv() #reading from .env
+import aiohttp
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()  # reading from .env
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
-intents.message_content = True # for reading messages
+intents.message_content = True  # for reading messages
 intents.members = True
 
 bot = commands.Bot(command_prefix="#", intents=intents, case_insensitive=True)
 
 ######################
 
-start_time = datetime.now()
+start_time = datetime.now(timezone.utc)
 
 ### terminal
 
+
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
+    print(f"Logged in as {bot.user} at {start_time}")
+
 
 ### events
 
 WELCOME_CHANNEL = 1516017173439582229
 
+
 @bot.event
 async def on_member_join(member):
-	channel = bot.get_channel(WELCOME_CHANNEL)
-	await channel.send(f"Welcome, {member.mention}!")
+    channel = bot.get_channel(WELCOME_CHANNEL)
+    await channel.send(f"Welcome, {member.mention}!")
+
 
 ### commands
 
+
 @bot.command()
 async def docs(ctx):
-	await ctx.send("""The cherrybot has the following features:
+    await ctx.send("""The cherrybot has the following features:
 	------------------------------------------
 	`#ping`
 	-->_replies 'pong!'; test for uptime_
@@ -62,270 +68,304 @@ async def docs(ctx):
 	`#createrole [hex] [role-name]`
 	-->_creates a new role with color and name_""")
 
+
 ### admin tool
 
+
 def is_admin(ctx):
-	with open("admins.json", "r") as f:
-		data = json.load(f)
+    with open("admins.json", "r") as f:
+        data = json.load(f)
 
-	guild_id = str(ctx.guild.id)
+    guild_id = str(ctx.guild.id)
 
-	if guild_id not in data:
-		return False
+    if guild_id not in data:
+        return False
 
-	admin_role_id = data[guild_id]
+    admin_role_id = data[guild_id]
 
-	for role in ctx.author.roles:
-		if str(role.id) == admin_role_id:
-			return True
+    for role in ctx.author.roles:
+        if str(role.id) == admin_role_id:
+            return True
 
-	return False
+    return False
+
 
 @bot.command()
 async def admin_add(ctx, role: discord.Role):
-	if ctx.author.id != ctx.guild.owner_id:
-		await ctx.send("Only the owner can use this command.")
-		return
-	with open("admins.json", "r") as f:
-		data = json.load(f)
+    if ctx.author.id != ctx.guild.owner_id:
+        await ctx.send("Only the owner can use this command.")
+        return
+    with open("admins.json", "r") as f:
+        data = json.load(f)
 
-	guild_id = str(ctx.guild.id)
+    guild_id = str(ctx.guild.id)
 
-	data[guild_id] = str(role.id)
+    data[guild_id] = str(role.id)
 
-	with open("admins.json", "w") as f:
-		json.dump(data, f)
+    with open("admins.json", "w") as f:
+        json.dump(data, f)
 
-	await ctx.send("Done! Added an admin role for the server.")
+    await ctx.send("Done! Added an admin role for the server.")
+
+
+### basic command work
+
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send("Pong!")
+    await ctx.send(f"Pong! My latency is {bot.latency}.")
+
 
 @bot.command()
 async def uptime(ctx):
-	delta = datetime.now() - start_time
-	await ctx.send(f"This instance has been running for {delta}.")
+    delta = datetime.now(timezone.utc) - start_time
+    await ctx.send(f"This instance has been running for {delta}.")
+
 
 @bot.command(name="67")
 async def sixtyseven(ctx):
-	await ctx.send("https://tenor.com/pU4XiE6POTB.gif")
+    await ctx.send("https://tenor.com/pU4XiE6POTB.gif")
+
 
 @bot.command()
 async def name(ctx, *, username=None):
-	if username is None:
-		await ctx.send("No name written. Try `#name Anna`.")
-		return
-	await ctx.send(f"hi, {username.strip().lower().capitalize()}")
-	if username.strip().lower() == "alex":
-		await ctx.send("thats a nice name btw!")
+    if username is None:
+        await ctx.send("No name written. Try `#name Anna`.")
+        return
+    await ctx.send(f"hi, {username.strip().lower().capitalize()}")
+    if username.strip().lower() == "alex":
+        await ctx.send("thats a nice name btw!")
+
 
 @bot.command()
 async def poker(ctx):
-	await ctx.send("Poker? I barely know her!")
+    await ctx.send("Poker? I barely know her!")
+
 
 @bot.command()
 async def remind(ctx, amount: int, unit: str, *, message="something!"):
-	unit = unit.strip().lower()
+    unit = unit.strip().lower()
 
-	if unit.startswith("min"):
-		seconds = amount * 60
-		if amount == 1:
-			temp = "minute"
-		else:
-			temp = "minutes"
-	elif unit.startswith("hour"):
-		seconds = amount * 3600
-		if amount == 1:
-			temp = "hour"
-		else:
-			temp = "hours"
-	elif unit.startswith("sec"):
-		seconds = amount
-		if amount == 1:
-			temp = "second"
-		else:
-			temp = "seconds"
-	else:
-		await ctx.send("That didnt work. Try using seconds, minutes or hours.")
-		return
+    if unit.startswith("min"):
+        seconds = amount * 60
+        if amount == 1:
+            temp = "minute"
+        else:
+            temp = "minutes"
+    elif unit.startswith("hour"):
+        seconds = amount * 3600
+        if amount == 1:
+            temp = "hour"
+        else:
+            temp = "hours"
+    elif unit.startswith("sec"):
+        seconds = amount
+        if amount == 1:
+            temp = "second"
+        else:
+            temp = "seconds"
+    else:
+        await ctx.send("That didnt work. Try using seconds, minutes or hours.")
+        return
 
-	await ctx.send(f"Will do that! See ya in {amount} {temp}!")
-	await asyncio.sleep(seconds)
-	await ctx.send(f"{ctx.author.mention}, reminder: {message}")
+    await ctx.send(f"Will do that! See ya in {amount} {temp}!")
+    await asyncio.sleep(seconds)
+    await ctx.send(f"{ctx.author.mention}, reminder: {message}")
+
 
 @bot.command()
-async def avatar(ctx, member: str = None):
-	if member is None:
-		found_member = ctx.author
-	else:
-		try:
-			found_member = await commands.MemberConverter().convert(ctx, member)
-		except:
-			found_member = discord.utils.get(ctx.guild.members, name=member)
+async def avatar(ctx, member: discord.Member = None):
+    if member is None:
+        member = ctx.author
 
-	if found_member is None:
-		await ctx.send("Couldnt find anyone.")
-		return
+    if member is None:
+        await ctx.send("Couldnt find anyone.")
+        return
 
-	await ctx.send(found_member.display_avatar.url)
+    await ctx.send(member.display_avatar.url)
+
+
+@avatar.error
+async def avatar_error(ctx, error):
+    if isinstance(error, commands.MemberNotFound):
+        await ctx.send("Could not find that person.")
+    else:
+        raise error
+
 
 ### to do list commands
 
+
 @bot.group(invoke_without_command=True)
 async def todo(ctx):
-	await ctx.send(
-		">>> Use #`todo add ...` to add a task \n"
-		"Use #`todo remove (index)` to remove a task \n"
-		"Use #`todo see` to see all your tasks"
-		)
+    await ctx.send(
+        ">>> Use #`todo add ...` to add a task \n"
+        "Use #`todo remove (index)` to remove a task \n"
+        "Use #`todo see` to see all your tasks"
+    )
+
+
 @todo.command(name="add")
 async def todo_add(ctx, *, task: str):
-	with open("todo.json", "r") as f:
-		data = json.load(f)
+    with open("todo.json", "r") as f:
+        data = json.load(f)
 
-	user_id = str(ctx.author.id)
+    user_id = str(ctx.author.id)
 
-	if user_id not in data:
-		data[user_id] = []
+    if user_id not in data:
+        data[user_id] = []
 
-	data[user_id].append(task)
+    data[user_id].append(task)
 
-	with open("todo.json", "w") as f:
-		json.dump(data, f)
+    with open("todo.json", "w") as f:
+        json.dump(data, f)
 
-	await ctx.send("Added your new task!")
+    await ctx.send("Added your new task!")
+
 
 @todo.command(name="remove")
 async def todo_remove(ctx, index: int):
-	with open("todo.json", "r") as f:
-		data = json.load(f)
-	user_id = str(ctx.author.id)
 
-	if user_id not in data or not data[user_id]:
-		await ctx.send("You don't have any tasks yet!")
-		return
+    with open("todo.json", "r") as f:
+        data = json.load(f)
+    user_id = str(ctx.author.id)
 
-	if index-1 >= len(data[user_id]) or index <= 0:
-		await ctx.send("Not a valid index! Try again.")
-		return
+    if user_id not in data or not data[user_id]:
+        await ctx.send("You don't have any tasks yet!")
+        return
 
-	data[user_id].remove(data[user_id][index-1])
+    if index - 1 >= len(data[user_id]) or index <= 0:
+        await ctx.send("Not a valid index! Try again.")
+        return
 
-	with open("todo.json", "w") as f:
-		json.dump(data, f)
+    data[user_id].remove(data[user_id][index - 1])
 
-	await ctx.send(f"Removed task {index}.")
+    with open("todo.json", "w") as f:
+        json.dump(data, f)
+
+    await ctx.send(f"Removed task {index}.")
+
 
 @todo.command(name="see")
 async def todo_see(ctx):
-	with open("todo.json", "r") as f:
-		data = json.load(f)
-	user_id = str(ctx.author.id)
+    with open("todo.json", "r") as f:
+        data = json.load(f)
+    user_id = str(ctx.author.id)
 
-	if user_id not in data or not data[user_id]:
-		await ctx.send("You don't have any tasks yet!")
-		return
+    if user_id not in data or not data[user_id]:
+        await ctx.send("You don't have any tasks yet!")
+        return
 
-	message = ""
-	for i in range(len(data[user_id])):
-		message += f"{i+1}) {data[user_id][i]} \n"
-	await ctx.send(message + "\nFeel free to add or remove tasks.")
+    message = ""
+    for i in range(len(data[user_id])):
+        message += f"{i + 1}) {data[user_id][i]} \n"
+    await ctx.send(message + "\nFeel free to add or remove tasks.")
+
 
 ### bird api work
 
+
 @bot.command()
 async def bird(ctx):
-    async with aiohttp.ClientSession() as session:
-        async with session.get("https://api.some-random-api.com/animal/bird") as response:
-            data = await response.json()
+    async with (
+        aiohttp.ClientSession() as session,
+        session.get("https://api.some-random-api.com/animal/bird") as response,
+    ):
+        data = await response.json()
 
     await ctx.send(data["image"])
     await ctx.send(data["fact"])
 
+
 ### reaction role system
+
 
 @bot.command()
 @commands.check(is_admin)
 async def createrole(ctx, hex_color: str, *, role_name: str):
-	hex_color = hex_color[1:]
-	try:
-		role_color = int(hex_color, 16)
-	except ValueError:
-		await ctx.send("Thats not an accepted color. Try again.")
-		return
-	await ctx.guild.create_role(name=role_name, colour=discord.Colour(role_color))
+    hex_color = hex_color[1:]
+    try:
+        role_color = int(hex_color, 16)
+    except ValueError:
+        await ctx.send("Thats not an accepted color. Try again.")
+        return
+    await ctx.guild.create_role(name=role_name, colour=discord.Colour(role_color))
+
 
 @bot.command()
 @commands.check(is_admin)
 async def announce(ctx, *, text: str):
-	await ctx.send(">>> " + text)
+    await ctx.send(">>> " + text)
+
 
 @bot.command()
 @commands.check(is_admin)
 async def addreact(ctx, msg_id: int, emoji: str, role: discord.Role):
-	message = await ctx.channel.fetch_message(msg_id)
-	msg_id = str(msg_id)
-	await message.add_reaction(emoji)
+    message = await ctx.channel.fetch_message(msg_id)
+    msg_id = str(msg_id)
+    await message.add_reaction(emoji)
 
-	with open("reaction_roles.json", "r") as f:
-		data = json.load(f)
+    with open("reaction_roles.json", "r") as f:
+        data = json.load(f)
 
-	if msg_id not in data:
-		data[msg_id] = {}
+    if msg_id not in data:
+        data[msg_id] = {}
 
-	data[msg_id][emoji] = role.id
+    data[msg_id][emoji] = role.id
 
-	with open("reaction_roles.json", "w") as f:
-		json.dump(data, f)
+    with open("reaction_roles.json", "w") as f:
+        json.dump(data, f)
+
 
 @bot.event
 async def on_raw_reaction_add(payload):
-	if payload.user_id == bot.user.id:
-		return
-	with open("reaction_roles.json", "r") as f:
-		data = json.load(f)
+    if payload.user_id == bot.user.id:
+        return
+    with open("reaction_roles.json", "r") as f:
+        data = json.load(f)
 
-	msg_id = str(payload.message_id)
+    msg_id = str(payload.message_id)
 
-	if msg_id not in data:
-		return
-	emoji = str(payload.emoji)
+    if msg_id not in data:
+        return
+    emoji = str(payload.emoji)
 
-	if emoji not in data[msg_id]:
-		return
+    if emoji not in data[msg_id]:
+        return
 
-	role_id = data[msg_id][emoji]
-	guild = bot.get_guild(payload.guild_id)
-	role = guild.get_role(int(role_id))
-	member = guild.get_member(payload.user_id)
+    role_id = data[msg_id][emoji]
+    guild = bot.get_guild(payload.guild_id)
+    role = guild.get_role(int(role_id))
+    member = guild.get_member(payload.user_id)
 
-	await member.add_roles(role)
+    await member.add_roles(role)
+
 
 @bot.event
 async def on_raw_reaction_remove(payload):
-	if payload.user_id == bot.user.id:
-		return
+    if payload.user_id == bot.user.id:
+        return
 
-	with open("reaction_roles.json", "r") as f:
-		data = json.load(f)
+    with open("reaction_roles.json", "r") as f:
+        data = json.load(f)
 
-	msg_id = str(payload.message_id)
+    msg_id = str(payload.message_id)
 
-	if msg_id not in data:
-		return
+    if msg_id not in data:
+        return
 
-	emoji = str(payload.emoji)
+    emoji = str(payload.emoji)
 
-	if emoji not in data[msg_id]:
-		return
+    if emoji not in data[msg_id]:
+        return
 
-	role_id = data[msg_id][emoji]
-	guild = bot.get_guild(payload.guild_id)
-	role = guild.get_role(int(role_id))
-	member = guild.get_member(payload.user_id)
+    role_id = data[msg_id][emoji]
+    guild = bot.get_guild(payload.guild_id)
+    role = guild.get_role(int(role_id))
+    member = guild.get_member(payload.user_id)
 
-	await member.remove_roles(role)
+    await member.remove_roles(role)
+
 
 ############
 
